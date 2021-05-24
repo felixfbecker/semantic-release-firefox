7e27696197b861e34bea5095045ceaa3e19fae6b
Felix Becker <felix.b@outlook.com>
2021-05-24 13:11:58 +0200

    build: fix autofix errors

--- src/publish.ts
+++ src/publish.ts
@@ -1,16 +1,16 @@
 import delay from 'delay'
-import  = require('marked')
+import marked = require('marked')
 // @ts-ignore
-import  = require('marked-terminal')
+import TerminalRenderer = require('marked-terminal')
 import { authenticator } from 'otplib'
 import retry from 'p-retry'
 import { Browser, launch } from 'puppeteer'
 // @ts-ignore
-import  = require('upndown')
+import HtmlToMdConverter = require('upndown')
 import { DEFAULT_SOURCES_ARCHIVE_PATH, SharedConfig } from './config'
 import { Logger, ReleaseInfo } from './semantic-release'
 
-export interface PublishConfig {
+export interface PublishConfig extends SharedConfig {
     /** Add-on slug as in the URL, i.e. https://addons.mozilla.org/en-US/firefox/addon/SLUG/ */
     addOnSlug: string
 
@@ -63,7 +63,7 @@ export const publishFirefoxExtension = async (
         }
         browser = await launch({ args })
         const page = await browser.newPage()
-        page.on('console', (message) => {
+        page.on('console', message => {
             logger.log('Console:', message.type().toUpperCase(), message.text())
         })
         const submitUrl = `${amoBaseUrl}/en-US/developers/addon/${addOnSlug}/versions/submit/`
@@ -176,7 +176,11 @@ export const publishFirefoxExtension = async (
                         logger.log('No upload status yet')
                     } else {
                         logger.log(
-                            'Upload progress: ' + (progress || '').replace('Cancel', '').replace(/\s+/g, ' ').trim()
+                            'Upload progress: ' +
+                                (progress || '')
+                                    .replace('Cancel', '')
+                                    .replace(/\s+/g, ' ')
+                                    .trim()
                         )
                     }
                     status = await page.evaluate(
